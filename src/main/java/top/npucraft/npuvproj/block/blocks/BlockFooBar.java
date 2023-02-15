0ded2c1489a6a64732babf7a373c1366b871cae8
zymelaii <13282135976@sina.cn>
2023-02-15 11:47:20 +0800

    feat: refine model of BlockFooBar

--- src/main/java/top/npucraft/npuvproj/block/blocks/BlockFooBar.java
+++ src/main/java/top/npucraft/npuvproj/block/blocks/BlockFooBar.java
@@ -5,33 +5,95 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockFooBar extends Block {
-	public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+	public static PropertyDirection PROPERTY_FACING;
+
+	static {
+		PROPERTY_FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+	}
 
 	public BlockFooBar() {
 		super(Material.ROCK);
 	}
 
+	/**
+	 * @note 碰撞检测是由射线追踪算法实现的，模型情形下，采取的检测模式是射线与单一碰撞箱相交测试。
+	 */
 	@Override
-    public boolean isOpaqueCube(IBlockState state) {
-        return false;
-    }
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		AxisAlignedBB northBB = new AxisAlignedBB(0.25, 0.0, 0.25, 0.75, 1.0, 0.8);
+		switch (state.getValue(PROPERTY_FACING).getHorizontalIndex()) {
+			case 2: {
+				return northBB;
+			}
+			case 0: {
+				return new AxisAlignedBB(northBB.minX, 0.0, 1.0 - northBB.maxZ, northBB.maxX, 1.0, 1.0 - northBB.minZ);
+			}
+			case 1: {
+				return new AxisAlignedBB(northBB.minZ, 0.0, northBB.minX, northBB.maxZ, 1.0, northBB.maxX);
+			}
+			case 3: {
+				return new AxisAlignedBB(1.0 - northBB.minZ, 0.0, 1.0 - northBB.minX, 1.0 - northBB.maxZ, 1.0, 1.0 - northBB.maxX);
+			}
+		}
+		return null;
+	}
 
 	@Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, FACING);
-    }
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, PROPERTY_FACING);
+	}
 
 	@Override
 	public IBlockState getStateFromMeta(int meta) {
-		EnumFacing facing = EnumFacing.getHorizontal(meta & 3);
-		return getDefaultState().withProperty(FACING, facing);
+		return getDefaultState().withProperty(PROPERTY_FACING, EnumFacing.getHorizontal(meta));
 	}
 
 	@Override
 	public int getMetaFromState(IBlockState state) {
-		return state.getValue(FACING).getHorizontalIndex();
+		return state.getValue(PROPERTY_FACING).getHorizontalIndex();
+	}
+
+	/**
+	 * @note 放置方块动作会在 Remote 和 Non-Remote 分别响应一次
+	 */
+	@Override
+	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY,
+			float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+		EnumFacing placerFacing = placer.getHorizontalFacing();
+		if (placer.isSneaking()) {
+			placerFacing = placerFacing.getOpposite();
+		}
+		return getStateFromMeta(meta).withProperty(PROPERTY_FACING, placerFacing);
+	}
+
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer,
+			ItemStack stack) {
+		if (!worldIn.isRemote) {
+			String message = "BlockFooBar was placed by " + placer.getName() + " with facing="
+					+ state.getValue(PROPERTY_FACING).name();
+			placer.sendMessage(new TextComponentString(message));
+		}
 	}
 }
